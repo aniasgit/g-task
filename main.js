5c8fc645eef64119359a87b92644f21654cd8c0a
Anna Kozik <anna.kozik@yahoo.pl>
2024-02-12 19:28:20 +0100

    add equalizer functionality

--- main.js
+++ main.js
@@ -2,22 +2,65 @@ const audioPlayer = document.querySelector("#audioPlayer");
 const audioInput = document.querySelector("#audioInput");
 const gridContainer = document.querySelector(".grid-container");
 
+let audioCtx, analyser, source;
+
 const createGrid = () => {
-  for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 6; j++) {
+  for (let i = 0; i < 8; i++) {
+    const column = document.createElement("div");
+    column.classList.add("column");
+
+    for (let j = 0; j < 8; j++) {
       const cell = document.createElement("div");
-      cell.setAttribute("id", `${i}_${j}`);
+      cell.setAttribute("id", j);
       cell.classList.add("cell");
-      gridContainer.appendChild(cell);
+      column.appendChild(cell);
     }
+
+    gridContainer.appendChild(column);
   }
 };
 
 const initializeEqualizer = () => {
-  const audioCtx = new AudioContext();
-  const analyser = audioCtx.createAnalyser();
+  if (audioCtx == undefined) {
+    audioCtx = new AudioContext();
+    analyser = audioCtx.createAnalyser();
+    source = audioCtx.createMediaElementSource(audioPlayer);
+    source.connect(analyser);
+    analyser.connect(audioCtx.destination);
+  }
 
-  //To be continued...
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  const columns = gridContainer.querySelectorAll(".column");
+  let columnCells = [];
+  columns.forEach((column) =>
+    columnCells.push(column.querySelectorAll(".cell"))
+  );
+
+  const draw = () => {
+    requestAnimationFrame(draw);
+
+    analyser.getByteFrequencyData(dataArray);
+    let barHeight = 0;
+
+    for (i = 0; i < bufferLength; i++) {
+      barHeight += dataArray[i] / 32;
+      if (i % 128 === 127) {
+        barHeight = Math.round(barHeight / 128);
+        columnCells[Math.floor(i / 128)].forEach((cell) => {
+          if (barHeight > +cell.id) {
+            cell.classList.add("fill");
+          } else {
+            cell.classList.remove("fill");
+          }
+        });
+        barHeight = 0;
+      }
+    }
+  };
+
+  draw();
 };
 
 const getFile = (event) => {
@@ -35,3 +78,4 @@ const getFile = (event) => {
 createGrid();
 
 audioInput.addEventListener("change", (event) => getFile(event));
+// audioPlayer.addEventListener("play", initializeEqualizer);
